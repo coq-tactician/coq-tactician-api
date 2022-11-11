1b545333a7cf7e351d4f69251d593fb1bcf43e48
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-11-11 16:27:09 +0100

    Try to locate include dir of capnproto

--- pytact/generate_api.py
+++ pytact/generate_api.py
@@ -6,8 +6,15 @@ import keyword
 from typing import Any
 from jinja2 import Environment, PackageLoader
 import inflection
+import shutil
 
 import capnp
+capnp_executable = shutil.which("capnp")
+if not capnp_executable:
+    raise ValueError("capnp executable not found")
+capnp_dir = os.path.dirname(capnp_executable)
+include_dir = [os.path.join(capnp_dir, "..", "include")]
+capnp.add_import_hook([include_dir])
 import schema_capnp
 
 def cython_name(name):
