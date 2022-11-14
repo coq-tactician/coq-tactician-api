5d3f779a76ff64023816ef0fb908afc71dd46cc8
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-11-14 15:51:38 +0100

    Fix

--- pytact/generate_api.py
+++ pytact/generate_api.py
@@ -13,7 +13,8 @@ capnp_executable = shutil.which("capnp")
 if not capnp_executable:
     raise ValueError("capnp executable not found")
 capnp_dir = os.path.dirname(capnp_executable)
-include_dir = [os.path.abspath(os.path.join(capnp_dir, "..", "include"))]
+include_dir = [os.path.abspath(os.path.join(capnp_dir, "..", "include")),
+               os.path.abspath(os.path.join(capnp_dir, "..", "include", "capnp"))]
 capnp.add_import_hook(include_dir)
 import schema_capnp
 
