0ac0d0873f5525e98c467707d1484fec5a8bcbb1
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-12-14 08:27:56 +0100

    Commit forgotten fake_coq_client.py

---
+++ pytact/fake_coq_client.py
@@ -0,0 +1,60 @@
+import socket
+import argparse
+import contextlib
+import signal
+from subprocess import Popen
+import capnp
+import pytact.graph_api_capnp as graph_api_capnp
+from pytact.data_reader import capnp_message_generator_from_file
+
+def run_fake_client(server_socket, messages_generator):
+    socket_reader = graph_api_capnp.PredictionProtocol.Response.read_multiple_packed(
+        server_socket, traversal_limit_in_words=2**64-1)
+    with contextlib.suppress(StopIteration):
+        msg = next(messages_generator)
+        while True:
+            msg.dynamic.as_builder().write_packed(server_socket)
+            prev_sig = signal.signal(signal.SIGINT, signal.SIG_DFL)  # SIGINT catching OFF
+            response = next(socket_reader, None)
+            signal.signal(signal.SIGINT, prev_sig)  # SIGINT catching ON
+            msg = messages_generator.send(response)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = 'A fake Coq client that connects to a prediction server and feeds it a stream of previously ' +
+                      'recorded messages.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('data',
+                        type=str,
+                        help='A file that contains a previously recorded communication sequence between Coq ' +
+                             'and a prediction server.')
+    connect_group = parser.add_mutually_exclusive_group(required=True)
+    connect_group.add_argument('--tcp',
+                        dest='tcp_location',
+                        type = str,
+                        default = None,
+                        help='Connect to a prediction server on the specified ip:port.')
+    connect_group.add_argument('--stdin',
+                        dest='executable',
+                        type = str,
+                        default = None,
+                        help='Start the specified prediction server and connect to it through stdin.')
+    cmd_args = parser.parse_args()
+
+    with open(cmd_args.data, 'rb') as message_file:
+        messages_generator = capnp_message_generator_from_file(message_file)
+        if cmd_args.tcp_location is not None:
+            addr, port = cmd_args.tcp_location.split(':')
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+                server_socket.connect((addr, int(port)))
+                run_fake_client(server_socket, messages_generator)
+        else:
+            our_sock, their_sock = socket.socketpair()
+            process = Popen(cmd_args.executable, shell=True, stdin=their_sock)
+            their_sock.close()
+            run_fake_client(our_sock, messages_generator)
+            our_sock.close()
+            process.communicate()
+
+if __name__ == '__main__':
+    main()
