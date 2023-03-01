84b4569db4ff6c442fd79056c3242f4c44e3c9f2
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-03-01 22:18:39 +0100

    Fix ocaml

--- datasets/current/stage1.sh
+++ datasets/current/stage1.sh
@@ -2,5 +2,5 @@
 # eval $(opam env --root=./opam-root --switch=default --set-root)
 opam switch create . --empty --repos=custom-archive=git+https://github.com/LasseBlaauwbroek/custom-archive.git,coq-extra-dev=https://coq.inria.fr/opam/extra-dev,coq-core-dev=https://coq.inria.fr/opam/core-dev,coq-released=https://coq.inria.fr/opam/released,default
 eval $(opam env --root=./opam-root --switch=default --set-root)
-time opam install --keep-build-dir --yes --assume-depext ./coq-tactician-reinforce/coq-tactician-reinforce.opam lwt.4.5.0 --fake
+time opam install --keep-build-dir --yes --assume-depext ./coq-tactician-reinforce/coq-tactician-reinforce.opam lwt.4.5.0 ocaml-base-compiler.4.09.1 --fake
 opam switch export opam-stage2 --freeze
