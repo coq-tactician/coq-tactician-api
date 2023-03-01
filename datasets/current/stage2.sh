7c10d9a0d208aa682d82352512886e8751cc5cf8
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-03-01 22:22:45 +0100

    Fix jsofocaml

--- datasets/current/stage2.sh
+++ datasets/current/stage2.sh
@@ -1,3 +1,3 @@
-eval $(opam env --root=./opam-root --switch=default --set-root)
+eval $(opam env --root=./opam-root --set-root)
 time opam install --yes --keep-build-dir coq-tactician-stdlib.8.11.dev --fake
 opam switch export opam-stage2 --freeze
