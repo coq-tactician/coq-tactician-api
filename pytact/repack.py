ecd1b2232a4cbd03b5b628d3921535fd041064b2
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-03-23 04:45:42 +0100

    Cleanup python code

--- pytact/repack.py
+++ pytact/repack.py
@@ -2,7 +2,7 @@ import sys
 import os
 from pathlib import Path
 import capnp
-import graph_api_capnp
+import pytact.graph_api_capnp as api
 
 max_node = {}
 node_total = 0
@@ -15,6 +15,6 @@ file_list = [f for f in rootdir.glob('**/*.bin') if f.is_file()]
 for f in file_list:
     print(f)
     fin = open(f)
-    g = graph_api_capnp.Graph.read_packed(fin, traversal_limit_in_words=2**64-1)
+    g = api.Graph.read(fin, traversal_limit_in_words=2**64-1)
     fout = open(f.with_suffix('.repacked'), 'w+b')
     g.as_builder().write(fout)
