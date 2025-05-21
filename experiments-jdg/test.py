332bf0464d4a47ba803ba05aafad519dce116441
huubdejong <huubdejong@hotmail.nl>
2025-05-21 08:46:48 -0700

    Added tactic analysis as notebook.

--- experiments-jdg/test.py
+++ experiments-jdg/test.py
@@ -3,14 +3,18 @@ from pathlib import Path
 from pytact.data_reader import data_reader
 import polars as pl
 import networkx
+import numpy as np
+import pandas as pd
 
 dataset_path = Path("/Users/huubdejong/Documents/Education/UBC/M2PI_2025/Project/TacticianDataTemp/TacticianDataTemp/TacticianDataTemp/v15-stdlib-coq8.11/dataset").resolve()
 
 
+errors = {} # for storing all the errors we encounter
+
 with data_reader(dataset_path) as data:
     # data is everything.
     # data.items() iterates over all the files in the dataset dictionary.
-    counts = {}
+    
     for idx, (relative_path,dataset) in enumerate(data.items()):
         data_lowlevel = dataset.lowlevel
         # type(www) is the lowevel pytact.graph_api_capnpn_cython.Dataset_Reader class
@@ -26,46 +30,42 @@ with data_reader(dataset_path) as data:
         # - has_module_name: true when the module_mame field is set
         # - module_name: the module name that the file resides in
         # - representative: int -- the official docs say that this is the entrypoint of the global context of of definitions available when this file is required by another.  If no 'super'-global definitions, then this is set to len(graph.nodes).  But it's not perfectly clear what that means.
-        print(dir(data_lowlevel))
-        print(type(data_lowlevel))
+        #print(dir(data_lowlevel))
+        #print(type(data_lowlevel))
 
         all_depIndices = [edge.target.dep_index for edge in data_lowlevel.graph.edges]
         all_depIndices_df = pl.DataFrame(all_depIndices)
 
         all_nodeIndices = [edge.target.node_index for edge in data_lowlevel.graph.edges]
         all_nodeIndices_df = pl.DataFrame(all_nodeIndices)
-        a = 0
+        
+        breakpoint()
 
-        for node in data_lowlevel.graph.nodes:
+        # This part iterates over the nodes and lists their children. 
+        for node in data_lowlevel.graph.nodes: # go through the list of nodes
+            break 
             children = []
-            a1 = node.children_index
-            a2 = node.children_count
-            for j in range(a2):
-                childIndex = data_lowlevel.graph.edges[a1 + j].target.node_index
-                children.append(data_lowlevel.graph.nodes[childIndex])
+            a1 = node.children_index # children_index tells you where (in the list of edges) the outgoing edges from our node start
+            a2 = node.children_count # children_count tells you how many elements starting from children_index you need to count
+            for j in range(a2): # for all children of node
+                childIndex = data_lowlevel.graph.edges[a1 + j].target.node_index 
+                try:
+                    children.append(data_lowlevel.graph.nodes[childIndex]) # try adding child to list of children
+                except IndexError as e: 
+                    errors["IndexError"] = str(e) # store error if there is one
             print("Node number " + str(node.identity) + " with children " + str([c.identity for c in children]))
             #breakpoint()
 
-        # may need to skip ssr or ssreflect ?
+       
         
-        for c in dataset.definitions(spine_only=True):
-            if c.proof != None:
-                for i in range(len(c.proof)):
-                    #print(c.proof[i].tactic.base_text)
-                    if c.proof[i].tactic.base_text in counts:
-                        counts[c.proof[i].tactic.base_text] += 1
-                    else:
-                        counts.update({c.proof[i].tactic.base_text: 1})
-                a = c
-        #print(a)
         
-        breakpoint()
+        
 
 # breakpoint() pauses execution so you can poke at stuff
 # dir(object) tells you everything you can call on the object
 
 
- 
+
 
 
 
