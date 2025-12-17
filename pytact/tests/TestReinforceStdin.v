061ccbc7111219956b04e5359a23dd896126ed11
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2025-12-18 03:04:42 +0800

    Simplified capability based prover

--- pytact/tests/TestReinforceStdin.v
+++ pytact/tests/TestReinforceStdin.v
@@ -1,5 +1,13 @@
-Goal True -> True.
-  intro. apply H.
+Definition comp {A} (f g : A -> A) (x: A) := f (g x).
+Definition mid {A} (x: A) := x.
+
+Goal forall (A : Prop), A -> A.
+  refine (fun (A : Prop) => _).
+  apply comp.
+  apply mid.
+  apply comp.
+  apply mid.
+  apply mid.
 Qed.
 
 Tactician Explore.
